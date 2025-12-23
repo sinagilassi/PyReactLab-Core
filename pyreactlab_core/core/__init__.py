2c89daac5b08aa5bbf899de8bf875f03635c6b9a
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-12-23 16:08:42 -0500

    add core functionality for chemical balance operations

---
+++ pyreactlab_core/core/__init__.py
@@ -0,0 +1,14 @@
+# import
+from .chem_balance import (
+    balance,
+    balance_algebraic,
+    balance_half_reaction,
+    balance_oxidation_number,
+)
+
+__all__ = [
+    "balance",
+    "balance_algebraic",
+    "balance_half_reaction",
+    "balance_oxidation_number",
+]
