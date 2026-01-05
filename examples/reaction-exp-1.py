ea9a606594a88a94c81be4e4a1c25292039a551f
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-01-05 16:51:05 -0500

    exp

--- examples/reaction-exp-1.py
+++ examples/reaction-exp-1.py
@@ -1,13 +1,51 @@
 # import libs
 from pyreactlab_core.models.reaction import Reaction
+from pythermodb_settings.models import Component
 from rich import print
 
-reaction_1 = Reaction(
-    name="Combustion of Methane",
-    reaction="CO2(g) + 3H2(g) => CH3OH(g) + H2O(g)"
+# NOTE: define components
+component_co2 = Component(
+    name="Carbon Dioxide",
+    formula="CO2",
+    state="g"
 )
 
-# print analysis
+component_h2 = Component(
+    name="Hydrogen",
+    formula="H2",
+    state="g"
+)
+
+component_ch3oh = Component(
+    name="Methanol",
+    formula="CH3OH",
+    state="g"
+)
+
+component_h2o = Component(
+    name="Water",
+    formula="H2O",
+    state="g"
+)
+
+components = [
+    component_co2,
+    component_h2,
+    component_ch3oh,
+    component_h2o
+]
+
+components = []
+components = None
+
+# NOTE: define components
+reaction_1 = Reaction(
+    name="Combustion of Methane",
+    reaction="CO2(g) + 3H2(g) => CH3OH(g) + H2O(g)",
+    components=components
+)
+
+# NOTE: print analysis
 print(
     f"[bold underline]Reaction Analysis for: {reaction_1.name}[/bold underline]")
 print(f"Reaction: {reaction_1.reaction}")
@@ -26,3 +64,6 @@ print(f"Reaction State: {reaction_1.reaction_state}")
 print(f"Carbon Count: {reaction_1.carbon_count}")
 print(f"Reactants Names: {reaction_1.reactants_names}")
 print(f"Products Names: {reaction_1.products_names}")
+print(f"All Components: {reaction_1.all_components}")
+print(f"Available components: {reaction_1.available_components}")
+print(f"Component Checker: {reaction_1.component_checker}")
