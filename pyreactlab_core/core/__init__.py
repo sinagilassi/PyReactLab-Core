96ce5244b2a111de7c988e442ab8c7f469bedc95
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-08-23 16:40:05 -0400

    Add chem react module imports and define __all__ for public API

--- pyreactlab_core/core/__init__.py
+++ pyreactlab_core/core/__init__.py
@@ -1,1 +1,17 @@
 
+# NOTE: chem react
+from .chem_react import (
+    ReactionMode,
+    PhaseRule,
+    Reactant,
+    Product,
+    ChemReact
+)
+
+__all__ = [
+    "ReactionMode",
+    "PhaseRule",
+    "Reactant",
+    "Product",
+    "ChemReact"
+]
