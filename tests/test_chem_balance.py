3a2af8e79434e242ffc910d2d1b18a7e73e42ac1
sina.gilassi <sina.gilassi@gmail.com>
2026-08-26 13:13:15 -0400

    Add tests for Reaction model auto-balancing and state preservation

--- tests/test_chem_balance.py
+++ tests/test_chem_balance.py
@@ -1,8 +1,10 @@
+from pyreactlab_core.configs.constants import ALL_REACTION_MODE_SYMBOLS
 from pyreactlab_core.docs.chem_balance import (
     _split_equation,
     balance,
     parse_species,
 )
+from pyreactlab_core.models.reaction import Reaction
 
 
 def test_trailing_plus_ion_is_not_split_as_separator():
@@ -23,11 +25,53 @@ def test_trailing_plus_ion_is_not_split_as_separator():
 
 def test_leading_coefficients_are_stripped_from_raw_string_species():
     assert balance("CO2 + H2 => 3CH3OH + H2O") == (
-        "CO2 + 3 H2 -> CH3OH + H2O"
+        "CO2 + 3 H2 => CH3OH + H2O"
     )
     assert balance("CO2(g) + H2(g) => 3CH3OH(g) + H2O(g)") == (
-        "CO2(g) + 3 H2(g) -> CH3OH(g) + H2O(g)"
+        "CO2(g) + 3 H2(g) => CH3OH(g) + H2O(g)"
     )
+
+
+def test_balance_reaction_object_preserves_states():
+    reaction = Reaction(
+        name="Hydrogen Combustion",
+        reaction="H2(g) + O2(g) => H2O(g)",
+    )
+
+    assert balance(reaction) == "2 H2(g) + O2(g) => 2 H2O(g)"
+
+
+def test_reaction_model_does_not_auto_balance_by_default():
+    reaction = Reaction(
+        name="Hydrogen Combustion",
+        reaction="H2(g) + O2(g) => H2O(g)",
+    )
+
+    assert reaction.reaction == "H2(g) + O2(g) => H2O(g)"
+    assert reaction.is_balanced is False
+
+
+def test_reaction_model_can_auto_balance_before_analysis():
+    reaction = Reaction(
+        name="Hydrogen Combustion",
+        reaction="H2(g) + O2(g) => H2O(g)",
+        balance_reaction=True,
+    )
+
+    assert reaction.reaction == "2 H2(g) + O2(g) => 2 H2O(g)"
+    assert reaction.reaction_stoichiometry == {
+        "H2-g": -2.0,
+        "O2-g": -1.0,
+        "H2O-g": 2.0,
+    }
+    assert reaction.is_balanced is True
+
+
+def test_balance_preserves_original_reaction_operator():
+    for operator in ALL_REACTION_MODE_SYMBOLS:
+        assert balance(f"H2 + O2 {operator} H2O") == (
+            f"2 H2 + O2 {operator} 2 H2O"
+        )
 
 
 def test_underdetermined_balance_keeps_all_input_species():
